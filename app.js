e3cb37b9cfeb305313c75f478cdbf25ef69be803
Sujay Paul <sujaypaul531@gmail.com>
2023-01-05 00:01:10 +0530

    project-complete

--- app.js
+++ app.js
@@ -3,22 +3,41 @@ const appsContainer = document.querySelector(".apps-container")
 const settingsBtn = document.querySelector(".settings")
 const settingsOptions = document.querySelector(".settings-options")
 
-appsContainer.innerHTML = apps.map((item, index)=>{
-     return (
-        `
+appsContainer.innerHTML = apps.map((item, index) => {
+  return (
+      `
        <div class="app app-${index + 1}">
         <img src=${item.logo}>
         <h1>${item.title}</h1>
        </div> 
-        `
-     )
+      `
+  )
 }).join("")
 
 
-appsToggle.addEventListener("click", (e) => {
+appsToggle.addEventListener("click", () => {
   appsContainer.classList.toggle("reveal")
 })
 
-settingsBtn.addEventListener("click", (e) => {
+settingsBtn.addEventListener("click", () => {
   settingsOptions.classList.toggle("reveal")
 })
+
+document.addEventListener("mousedown", (e) => {
+  if (e.target != appsToggle) {
+    console.log("apps");
+    if (appsContainer.classList.contains("reveal")) {
+      appsContainer.classList.remove("reveal")
+    }
+  }
+  if(e.target != settingsBtn){
+    console.log("settings");
+    if (settingsOptions.classList.contains("reveal")) {
+      settingsOptions.classList.remove("reveal")
+    }
+  }
+})
+
+
+
+
